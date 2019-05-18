457aaa7202b9481e4d311198c994c9f87ca44c7a
daihyun <judaihyun1@gmail.com>
2019-05-18 20:07:25 +0900

    TODO : fade out, timeIndicator

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
+'use strict'
+
 chrome.runtime.onInstalled.addListener(function(){
   alert('installed'); 
+  chrome.tabs.executeScript({
+    file: "timeTimer.js"
+  });
+
+  
 });
+
--- timeTimer.js
+++ timeTimer.js
@@ -1,52 +1,57 @@
 
-'use strict'
 
 var timeTimer = timeTimer || {};
 
-
 function starter()
 {
-    var timerObj = new timeTimer();
-    timerObj.initTimeTimer(150);
+    const timerObj = new timeTimer();
+    let radius = 150;
+    timerObj.setDefaultTime(6);
+    timerObj.setRadius(radius);
+    timerObj.start();
 }
 
 
 document.addEventListener('DOMContentLoaded',function(){
+'use strict'
 
-   
 timeTimer = function()
 {
 
-    var state    = Object.create(null);
-    var view     = Object.create(null);
-    var controls = Object.create(null);
+    const state    = Object.create(null),
+          view     = Object.create(null),
+          controls = Object.create(null),
+          error = Object.create(null),
+          ms = 1000;
 
-    var CLOCKFACE_STROKE_STYLE = 'red',
-        CLOCKFACE_CENTER_RADIUS =  5,
-        CLOCKFACE_RADIUS = 120,
-        CLOCKNUMBER_STROKE_STYLE = 'black',
-        CLOCKNUMBER_TEXT_ALIGN = 'center',
-        TICK_SHORT_STROKE_STYLE = 'black',
-        TICK_WIDTH = 10,
+    const CLOCKFACE_STROKE_STYLE = 'red',
+          CLOCKFACE_CENTER_RADIUS =  5,
+          CLOCKFACE_RADIUS = 120,
+          CLOCKNUMBER_STROKE_STYLE = 'black',
+          CLOCKNUMBER_TEXT_ALIGN = 'center',
+          CLOCKNUMBER_RATIO = 0.13,
+          TICK_SHORT_STROKE_STYLE = 'black',
+          TICK_WIDTH = 10,
 
-        MAX_TIME_COUNT = 60;
+          ANGLE_DELTA = (Math.PI * 2) / 60,
+          MAX_TIME_COUNT = 60;
 
 
 
-view.create = function(radius)
+view.create = radius =>
 {
     view.radius = radius;
 
     view.canvas = elt('canvas', {id:'canvas', width:'350', height:'400'});
     view.circle = {
-        x : this.canvas.width / 2,
-        y : this.canvas.height / 2
+        x : view.canvas.width / 2,
+        y : view.canvas.height / 2 + 0.5
     }
 
     if(view.ctx) delete view.ctx;
     view.ctx = view.canvas.getContext('2d')
 
-    view.timeDiv = elt('div', {id:'displayTime'});
+    view.timeIndicator = elt('div', {id:'displayTime'});
 
     view.drawClockFace(); 
 
@@ -56,41 +61,67 @@ view.create = function(radius)
 
     view.drawDefaultInnerCircle();
 
+    //view.drawGrid('gray', 10, 10);
+    
+
     view.updateClockEvent = new Event('updateClock');
     document.addEventListener('updateClock',view.drawInnerCircle);
 
-
-
-
-    var pos = document.getElementById('pos');
+    /*
+    const pos = document.getElementById('pos');
     view.canvas.addEventListener('mousedown',function(e){
     
-    var tt = windowToCanvas(e.clientX, e.clientY);
+    const tt = windowToCanvas(e.clientX, e.clientY);
         pos.innerText = tt.x + ',' +tt.y;
     },false);
     
     function windowToCanvas(x, y)
     {
-    var bbox = view.canvas.getBoundingClientRect();
-    return { x: x - bbox.left * (view.canvas.width / bbox.width),
-      y: y - bbox.top * (view.canvas.height / bbox.height) };
+        const bbox = view.canvas.getBoundingClientRect();
+        return { x: x - bbox.left * (view.canvas.width / bbox.width),
+        y: y - bbox.top * (view.canvas.height / bbox.height) };
     }
-
-
+    */
 
 
     return elt(
-        'div', { class:'mainLayout' }, view.canvas, view.timeDiv
+        'div', { class:'mainLayout' }, view.canvas, view.timeIndicator
     );
 }
 
+view.drawGrid = (color, stepx, stepy) =>
+{
+    view.ctx.save();
+    view.ctx.strokeStyle = color;
+    view.ctx.lineWidth = 0.5;
+    
+    for(let i = stepx + 0.5; i < view.ctx.canvas.width; i += stepx)
+    {
+        view.ctx.beginPath();
+        view.ctx.moveTo(i, 0);
+        view.ctx.lineTo(i, view.ctx.canvas.height);
+        view.ctx.stroke();
+    }
 
-view.drawClockFace = function()
+   
+    for(let i = stepy + 0.5; i < view.ctx.canvas.height; i += stepy)
+    {
+        view.ctx.beginPath();
+        view.ctx.moveTo(0, i);
+        view.ctx.lineTo(view.ctx.canvas.width, i);
+        view.ctx.stroke();
+    }
+   
+
+    view.ctx.restore();
+}
+
+
+view.drawClockFace = () =>
 {
     view.ctx.save();
     view.ctx.beginPath();
     view.ctx.strokeStyle = CLOCKFACE_STROKE_STYLE;
-    //arc(x, y, radius, startAngle, endAngle, anti)
     view.ctx.arc(view.circle.x, view.circle.y,
          CLOCKFACE_RADIUS, 0, 360, false);
     view.ctx.stroke();
@@ -102,19 +133,14 @@ view.drawClockFace = function()
     view.ctx.restore();
 }
 
-
-
-view.drawTicks = function()
+view.drawTicks = () =>
 {
-    var radius = CLOCKFACE_RADIUS,
+    const radius = CLOCKFACE_RADIUS,
         ANGLE_MAX = Math.PI * 2,
-        ANGLE_DELTA = Math.PI / 30,  // tick angle (180도 / 갯수) == 1파이이므로
-        TICK_WIDTH;
-
-    //console.log(`drawTicks() - radius : ${radius}, angleMax : ${ANGLE_MAX}, angleDelta : ${ANGLE_DELTA}`);
+        ANGLE_DELTA = Math.PI / 30;  // tick angle (180도 / 갯수) == 1파이이므로
 
     view.ctx.save();
-    for(var angle = 0, cnt = 0; angle < ANGLE_MAX;
+    for(let angle = 0, cnt = 0; angle < ANGLE_MAX;
                                 angle += ANGLE_DELTA, cnt+=2)
     {
         view.drawTick(angle, radius, cnt);
@@ -123,17 +149,17 @@ view.drawTicks = function()
 
 }
 
-view.drawTick = function(angle, radius, cnt)
+view.drawTick = (angle, radius, cnt) =>
 {
     //console.log('drawTick() - angle : ' + angle + ', radius : ' + radius + ', cnt : ' + cnt);
-    var tickWidth = cnt % 5 === 0 ? TICK_WIDTH : TICK_WIDTH / 2;
+    let tickWidth = cnt % 5 === 0 ? TICK_WIDTH : TICK_WIDTH / 2;
     view.ctx.save();
     view.ctx.beginPath();
-    var moveX = view.circle.x + Math.cos(angle) * (radius - tickWidth);
-    var moveY = view.circle.y + Math.sin(angle) * (radius - tickWidth);
+    let moveX = view.circle.x + Math.cos(angle) * (radius - tickWidth);
+    let moveY = view.circle.y + Math.sin(angle) * (radius - tickWidth);
 
-    var lineX = view.circle.x + Math.cos(angle) * (radius);
-    var lineY = view.circle.y + Math.sin(angle) * (radius);
+    let lineX = view.circle.x + Math.cos(angle) * (radius);
+    let lineY = view.circle.y + Math.sin(angle) * (radius);
 
     //console.log('lineX : ' + lineX +', lineY : ' + lineY);
     //console.log('moveX : ' + moveX +', moveY : ' + moveY);
@@ -145,57 +171,60 @@ view.drawTick = function(angle, radius, cnt)
     view.ctx.stroke();
     view.ctx.restore();
 }
-view.drawDefaultInnerCircle = function()
+
+view.drawDefaultInnerCircle = () =>
 {
+    console.log('drawDefaultInnerCircle - defaultTime : ' + state.defaultTime);
+    let angle = ( Math.PI * 1.5) + ANGLE_DELTA * (state.defaultTime / ms);
+    
     view.ctx.save();
     view.ctx.beginPath();
     view.ctx.strokeStyle = 'blue';
     view.ctx.fillStyle = 'blue';
-    view.ctx.arc(view.circle.x, view.circle.y,
-                 CLOCKFACE_RADIUS - TICK_WIDTH,
-                 0, Math.PI * 2);
-    view.ctx.fill();
-    view.ctx.restore(); 
-}
-
-view.drawInnerCircle = function(e)
-{
-    //console.log(e.detail);
-    var ANGLE_DELTA = (Math.PI * 2) / 60;
-    var angle = (Math.PI * 1.5) - ANGLE_DELTA * e.detail;
-    var degree = (180/Math.PI) * angle;
-    
-    view.ctx.save();
-    view.ctx.beginPath();
-    view.ctx.strokeStyle = 'white';
-    view.ctx.fillStyle = 'white';
+    console.log('angle : ' + angle);
+    console.log('delta : ' + ANGLE_DELTA);
     view.ctx.moveTo(view.circle.x, view.circle.y);
     view.ctx.arc(view.circle.x, view.circle.y,
                  CLOCKFACE_RADIUS - TICK_WIDTH,
+                 Math.PI * 1.5, angle);
+    view.ctx.fill();
+    view.ctx.restore(); 
+
+}
+
+view.drawInnerCircle = (evt) =>
+{
+    let angle = (Math.PI * 1.5) - ANGLE_DELTA * evt.detail;
+    let degree = (180 / Math.PI) * angle;
+
+    view.ctx.save();
+    view.ctx.beginPath();
+    view.ctx.fillStyle = 'rgba(255, 255, 255, .2)';
+    view.ctx.moveTo(view.circle.x, view.circle.y);
+    view.ctx.arc(view.circle.x, view.circle.y,
+                 CLOCKFACE_RADIUS - TICK_WIDTH + 0.5,
                  Math.PI * 1.5, angle, true);
-    console.log('degree : ' + degree);
-    console.log(`angle : ${angle}, radius : ${CLOCKFACE_RADIUS - TICK_WIDTH}`);
+    //console.log('degree : ' + degree);
+    //console.log(`angle : ${angle}, radius : ${CLOCKFACE_RADIUS - TICK_WIDTH}`);
+    view.ctx.closePath();
     view.ctx.fill();
     view.ctx.restore();
 }
 
-view.drawClockNumber = function()
+view.drawClockNumber = () =>
 {
-    var ang;
-    var num;
-    var radius = view.radius;
+    let radius = view.radius - 10;
     view.ctx.save();
     view.ctx.translate(view.circle.x, view.circle.y);
-    view.ctx.font = radius*0.15 + "px arial";
+    view.ctx.font = (radius * CLOCKNUMBER_RATIO) + "px arial";
     view.ctx.textBaseline = "middle";
     view.ctx.textAlign = CLOCKNUMBER_TEXT_ALIGN;
     view.ctx.strokeStyle = CLOCKNUMBER_STROKE_STYLE;
-    console.log('radius : ' + radius);
-    var realNum = 0;
-    for(num = 0; num < 12; num++)
+
+    let realNum = 0;
+    for(let num = 0; num < 12; num++)
     {
-    
-        ang = num * Math.PI / 6;
+        let ang = num * Math.PI / 6;
         view.ctx.rotate(ang);
         view.ctx.translate(0, -radius);
         view.ctx.rotate(-ang);
@@ -209,114 +238,147 @@ view.drawClockNumber = function()
     view.ctx.restore();
 }
 
-state.create = function(_endTime)
+state.create = () =>
 {
     state.timerId;
     state.isStart = false;
-    state.defaultTime = _endTime;
-    state.endTime = _endTime;
-    state.startStopButton;
-    state.restartBtn;
 
-    state.restartEvent = new Event('resTrigger');
-    document.addEventListener('resTrigger',resTrigger);
-   
+    state.restartEvent = new Event('restartEvent');
+    document.addEventListener('restartEvent', state.restartEventProcess);
 }
 
- 
-function resTrigger()
+
+state.clearInterval = () =>
 {
-    console.info(`resTrigger Event - activate ( timer_id : ${state.timerId} , ${state.isStart} )`);
-    if(state.isStart){
-        clearInterval(state.timerId);
-        state.restartBtn.style.visibility = 'hidden';
-        state.displayTime.innerHTML = state.defaultTime;
-        state.startStopButton.innerHTML = 'START';
-        state.isStart = false
-    }else{
-        state.restartBtn.style.visibility = 'visible';
-        clearInterval(state.timerId);
-        state.displayTime.innerHTML = state.defaultTime;
-    }
+    clearInterval(state.timerId);
+    state.endTime = state.defaultTime;
+    //state.displayTime.innerHTML = state.defaultTime;
 }
-state.updateClock = function(elapseTime)
+
+state.stopInterval = () =>
 {
-    /*
-    document.dispatchEvent(view.updateClockEvent,
-       {detail: elapseTime} );
-       */
+    clearInterval(state.timerId);
+}
+
+state.updateClock = elapseTime => 
+{
     document.dispatchEvent(new CustomEvent('updateClock',
      { detail: (MAX_TIME_COUNT - Math.floor(elapseTime)) }));
-
 }
 
-state.interval = function(t_unit)
+state.interval = t_unit =>
 {
     if(t_unit < 0) return;
-    const adjustInterval = 10;
+    const adjustInterval = 1000;
     state.displayTime = document.getElementById('displayTime');
-    var expectTime = Date.now() + t_unit;
-    var elapseTime;
+    let expectTime = Date.now() + t_unit;
+    let elapseTime;
+
     console.time();
     state.timerId = setInterval(adjustTime, Math.max(0, adjustInterval - elapseTime));
     console.info(`state.interval() - activate ( t_unit : ${t_unit}, timerId : ${state.timerId} )`);
+
     function adjustTime()
     { 
         elapseTime = Date.now() - expectTime;
         state.endTime = Math.abs(elapseTime);
-        var temp = Math.abs(elapseTime) / 1000;
-        state.updateClock(temp);
-        state.displayTime.innerHTML = temp;
+        let formattedTime = Math.abs(elapseTime) / 1000;
+     //   state.displayTime.innerHTML = formattedTime;
 
         if(elapseTime > 0)
         {
-            state.updateClock(temp);
-            state.displayTime.innerHTML = temp;
-            console.timeEnd();
-            state.endTime = state.defaultTime;
-            clearInterval(state.timerId);
+            state.completed(formattedTime);
             return;
         } 
         elapseTime += adjustInterval;
+        state.updateClock(formattedTime);
     }
 }
 
-state.startEventProcess = function()
+state.completed = (formattedTime) =>
 {
+    console.info('state.completed() ');
+    state.updateClock(formattedTime);
+    //state.displayTime.innerHTML = formattedTime;
+    console.timeEnd();
+    state.endTime = state.defaultTime;
+    state.imgToggle(state.restartBtn);
+    state.clearInterval();
+    state.stopEventProcess();
+    view.drawDefaultInnerCircle();
+}
+
+state.imgToggle = function(ele)
+{
+    let currentStyle = ele.style;
+    if(state.endTime > 0 && ele.className === "restart-btn"){
+        currentStyle.visibility = 'visible';
+        return; 
+    }
+    currentStyle.visibility = currentStyle.visibility == 'hidden' ? 'visible' : 'hidden';
+}
+
+state.restartEventProcess = () =>
+{
+    console.info(`restart Event - activate ( timer_id : ${state.timerId} , ${state.isStart} )`);
+    if(state.isStart)
+    {
+        view.drawDefaultInnerCircle();
+        state.imgToggle(state.restartBtn);
+        state.imgToggle(state.stopButton);
+        state.imgToggle(state.startButton);
+        state.isStart = false;
+        state.clearInterval();
+    }
+}
+
+state.startEventProcess = () =>
+{
+    if(state.isStart) return;
     console.info('state.startEventProcess() - activate');
     state.interval(state.endTime);
-    state.startStopButton.innerHTML = 'STOP';
-    state.restartBtn.style.visibility = 'visible';
+    state.imgToggle(state.startButton);
+    state.imgToggle(state.stopButton);
+    state.imgToggle(state.restartBtn);
     state.isStart = true;
 }
 
-state.stopEventProcess = function()
+state.stopEventProcess = () =>
 {
+    console.log('clicked');
+    if(!state.isStart) return;
     console.info('state.stopEventProcess() - activate ( timer_id : ' + state.timerId + ')');
-    clearInterval(state.timerId);
-    state.startStopButton.innerHTML = 'START';
+    state.stopInterval();
+    state.imgToggle(state.startButton);
+    state.imgToggle(state.stopButton)
     state.isStart = false;
 }
 
-controls.startAndStop = function(){
-    state.startStopButton = elt('button',{type:'button', id:'switch'},'START');
-    state.startStopButton.addEventListener('click', function(){
-    console.info('controls.startAndStop() - startStopButton clicked');
+controls.start = () =>
+{
+    state.startButton = elt('img',{class:'start-btn',
+                                   src:'images/startBtn.png',
+                                   style:'visibility:visible;'});
 
-        if(!state.isStart)
-        {
-            state.startEventProcess();
-        }else{
-            state.stopEventProcess(); 
-        }
-
-    });
-    return state.startStopButton;
+    state.startButton.addEventListener('click', state.startEventProcess);
+    return state.startButton;
 }
 
-controls.restart = function(){
-    state.restartBtn = elt('button',{type:'button', id:'restart'},'RESTART');
-    state.restartBtn.style.visibility = 'hidden';
+controls.stop = () =>
+{
+    state.stopButton = elt('img',{class:'stop-btn',
+                                  src:'images/stopBtn.png',
+                                  style:'visibility:hidden;'});
+
+    state.stopButton.addEventListener('click', state.stopEventProcess);
+    return state.stopButton;
+}
+
+controls.restart = () =>
+{
+    state.restartBtn = elt('img',{class:'restart-btn',
+                                 src:'images/restartBtn.png',
+                                 style:'visibility:hidden;'});
 
     state.restartBtn.addEventListener('click', function(){
         console.info('controls.restart() - restartBtn clicked');
@@ -326,25 +388,45 @@ controls.restart = function(){
     return state.restartBtn;
 }
 
-return{
-    initTimeTimer : function(radius)
+error.validator = (value) =>
+{
+    if(typeof value === 'undefined' && !value 
+    || Number.isNaN(value) || value < 0)
     {
-        console.info(`initTimeTimer is activated `);
-        var ms = 1000;
+        console.error('arguments is invaild or undefined');
+        delete this;
+        return;
+    }
+}
 
-        var viewPannel = view.create(radius);
+return{
+    setDefaultTime : (defaultTime) =>
+    {
+        error.validator(defaultTime);
+        defaultTime *= ms;
+        state.defaultTime = defaultTime;
+        state.endTime = defaultTime;
+    },
+    setRadius : (radius) =>
+    {
+        error.validator(radius);
+        state.radius = radius;
+    },
+    start : () =>
+    {
+        error.validator(state.radius);
+        error.validator(state.defaultTime);
+        state.create();
+        let viewPannel = view.create(state.radius);
 
-        state.create(60 * ms);
-
-        var toolbar = elt('div', {class:'toolbar'});
-        for(var name in controls)
+        let toolbar = elt('div', {class:'toolbar'});
+        for(let name in controls)
             toolbar.appendChild(controls[name]());
 
         viewPannel.appendChild(toolbar);
 
-      //document.body.appendChild(viewPannel);
-        
         document.body.insertBefore(viewPannel, document.body.firstChild);
+        console.info(`initTimeTimer is completely initailized`);
     }
 }
 
